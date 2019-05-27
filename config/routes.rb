028a3cff0c4db8dcecd0688152ccd9e4c8be623c
CamilleRungette <c.rungette@gmail.com>
2019-05-27 14:54:57 +0200

    aws development ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   resources :items do
   	 resources :avatars, only: [:create]
   	end
-
-  devise_for :users
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
