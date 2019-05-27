fec4b5b03026a617f634198d4cf4eb454d2317ce
CamilleRungette <c.rungette@gmail.com>
2019-05-27 16:53:44 +0200

    page show index done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-  	@item = Item.all
+  	@item = Item.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Item.destroy_all
 
 20.times do
   Item.create!(
-    title: Faker::Creature::Cat.name + Faker::Creature::Cat.name,
+    title: Faker::Creature::Cat.name + " " + Faker::Creature::Cat.name,
     description: "lorem ipsum",
     price: rand(0..100).to_f,
     image_url: "https://images.unsplash.com/photo-1472491235688-bdc81a63246e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
