fbda8fe2529332728a95c4769be5ea6ab8fd2412
RyanZidago <ryanzidago@gmail.com>
2019-05-28 17:15:58 +0200

    I think we fixed the bug ???

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :join_table_cart_item, dependent: :destroy
   has_many :items, through: :join_table_cart_item
 end
