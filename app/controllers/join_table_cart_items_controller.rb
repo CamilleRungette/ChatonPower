45b2198deb84b3f302328ba284329a794b2fbbaa
Van Steven Nguyen <nvguyen@gmail.com>
2019-05-28 16:43:05 +0200

    delete button

--- app/controllers/join_table_cart_items_controller.rb
+++ app/controllers/join_table_cart_items_controller.rb
@@ -1,6 +1,2 @@
 class JoinTableCartItemsController < ApplicationController
-  def destroy
-    @item = JoinTableCartItem.where(cart_id: params[:user_id]).find(1).destroy
-    redirect_to root_path
-  end
 end
