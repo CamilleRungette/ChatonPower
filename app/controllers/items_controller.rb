fa18526284730952f24ba5bc25c80c9fbdc26816
Van Steven Nguyen <nvguyen@gmail.com>
2019-05-28 16:13:51 +0200

    delete method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
   	@item = Item.find(params[:id])
   end
 
+  def destroy
+    @item = User.find(current_user.id).cart.items.find(params[:id])
+    @cart = JoinTableCartItem.where(cart_id: current_user.cart).find_by(item_id: @item.id)
+    @cart.destroy
+    redirect_to user_carts_path(current_user.id)
+  end
 end
--- app/controllers/join_table_cart_items_controller.rb
+++ app/controllers/join_table_cart_items_controller.rb
@@ -1,2 +1,6 @@
 class JoinTableCartItemsController < ApplicationController
+  def destroy
+    @item = JoinTableCartItem.where(cart_id: params[:user_id]).find(1).destroy
+    redirect_to root_path
+  end
 end
