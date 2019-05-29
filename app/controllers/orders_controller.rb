871ae368e0b347f26b1dadbd50d5203e8f158a28
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-05-29 15:22:46 +0200

    stripe ok without charges controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,38 @@
 class OrdersController < ApplicationController
 
+  def new
+    @items = User.find(current_user.id).cart.items
+    @amount = 0 
+    @items.each do |item| 
+      @amount += item.price
+    end 
+  end
+
+  def create
+    @items = User.find(current_user.id).cart.items
+    @amount = 0 
+    @items.each do |item| 
+      @amount += item.price
+    end 
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount.to_i*100,
+      description: 'Rails Stripe customer',
+      currency: 'eur',
+    })
+  redirect_to user_cart_order_path(current_user.id, current_user.cart.id, current_user.id)
+    
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
   def show
     @user = current_user
     @cart = current_user.cart
